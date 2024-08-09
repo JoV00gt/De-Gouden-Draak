2cfe5ac0e81f50b7f3a00831200941c72a7b8581
Barthol1 <83116011+Barthol1@users.noreply.github.com>
2024-08-09 11:58:00 +0200

    frontendtext

--- dev/gouden_draak/resources/views/home.blade.php
+++ dev/gouden_draak/resources/views/home.blade.php
@@ -1,3 +1,18 @@
 <x-homelayout>
-    <h1 class="text-lg">Magna do ex ex sint. Veniam qui veniam magna laboris ad esse non nostrud velit incididunt laborum fugiat in magna. Tempor occaecat occaecat ad commodo et. Eiusmod ex pariatur duis dolore officia in culpa id. Ipsum exercitation aliquip elit voluptate sint adipisicing minim eu irure ad dolore. Nisi eu eu eu amet non reprehenderit.</h1>
+    <div class="text-bold  text-center">
+        <p class="mb-4">Al jaren is De Gouden Draak een begrip als het gaat om de beste afhaalgerechten in 's-Hertogenbosch.
+            Graag trakteren we u op authentieke gerechten uit de Cantonese keuken.</p>
+        <h1 class="underline text-lg mb-2">Speciale Studentenaanbieding</h1>
+        <h1 class="underline text-2xl">Chinese Rijsttafel (2 personen)
+        </h1>
+        <p>Maak een keuze uit 3 van onderstaande keuzegerechten: <br><br>
+
+            Koe Loe Yuk Foe Yong Hai <br>
+            Tjap Tjoy Garnalen met Gebakken Knoflook <br>
+            Babi Pangang Kipfilet in Zwarte Bonen saus <br><br>
+
+            Met witte rijst. (Nasi of bami voor meerprijs mogelijk.)</p>
+        <h1 class="text-2xl">Prijs: â‚¬21,00</h1>
+    </div>
+
 </x-homelayout>
--- dev/gouden_draak/routes/web.php
+++ dev/gouden_draak/routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\MenuController;
+use App\Http\Controllers\CustomerMenuController;
 use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'Home'])->name('Home');
 Route::get('/contact', [ContactController::class, 'index'])->name('index');
 Route::get('/news', [NewsController::class, 'index'])->name('index');
-Route::get('/menu', [MenuController::class, 'index'])->name('index');
+Route::get('/menu', [CustomerMenuController::class, 'index'])->name('index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
