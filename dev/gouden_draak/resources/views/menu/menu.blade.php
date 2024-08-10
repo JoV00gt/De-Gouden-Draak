b58d08ea43c2bbe57d5a10f4a4cff8cb4e906977
Barthol1 <83116011+Barthol1@users.noreply.github.com>
2024-08-10 14:06:50 +0200

    final commit

---
+++ dev/gouden_draak/resources/views/menu/menu.blade.php
@@ -0,0 +1,4 @@
+<x-homelayout>
+    <img class="object-cover" src="images/menu/restaurant-menukaart-1.jpg" alt="">
+    <img class="object-cover" src="images/menu/restaurant-menukaart-1-2.jpg" alt="">
+</x-homelayout>
