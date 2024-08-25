b8a7217d9e8053b963034dc3b115faab8e7243f7
JosVoogt2107 <j.voogt1@student.avans.nl>
2024-08-25 18:43:34 +0200

    Update home.blade.php

--- dev/gouden_draak/resources/views/home.blade.php
+++ dev/gouden_draak/resources/views/home.blade.php
@@ -1,12 +1,15 @@
 <x-home-layout>
     <div class="font-bold p-10 text-center">
-        <h2 class="mb-4">{{__('introduction')}}</h2>
-        <h1 class="underline text-lg mb-2">{{__('deals-special-title')}}</h1> 
-        <h1 class="underline text-2xl">{{$deal->dish->name}}
-        </h1>
-        <p>{{__('valid')}} {{date('d-m-Y', strtotime($deal->start_date))}} {{__('till')}} {{date('d-m-Y', strtotime($deal->expire_date))}}.<br>
-        {{__('price_before')}} € {{number_format($deal->dish->price, 2, ',', '.')}} {{__('price_after')}} <b>€ {{ number_format($deal->price, 2, ',', '.') }}</b>
-        </p>
+        <p class="mb-4">{{__('introduction')}}</p>
+        <h2 class="underline text-lg mb-2">{{__('deals-special-title')}}</h2>
+        @if($deal)
+            <h2 class="underline text-2xl">{{$deal->dish->name}}</h2>
+            <p>{{__('valid')}} {{date('d-m-Y', strtotime($deal->start_date))}} {{__('till')}} {{date('d-m-Y', strtotime($deal->expire_date))}}.<br>
+                {{__('price_before')}} € {{number_format($deal->dish->price, 2, ',', '.')}} {{__('price_after')}} <b>€ {{ number_format($deal->price, 2, ',', '.') }}</b>
+            </p>
+        @else 
+            <p>{{__('no-results')}}</p>
+        @endif
     </div>
 
 </x-home-layout>
